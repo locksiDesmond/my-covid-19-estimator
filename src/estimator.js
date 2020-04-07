9e9ac62f386d70781eacd224c0b2992ea7ff1356
LocksiDesmond <Des.olanrewaju@gmail.com>
2020-04-07 21:12:44 +0100

    solved challenge 1(first part)

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,20 @@
+const currentlyInfected = (data) => {
+  const Inputdata = data;
+  const { reportedCases } = Inputdata;
+  const impactCurrentlyInfected = reportedCases * 10;
+  const severeCurrentlyInfected = reportedCases * 50;
+  return {
+    impact: {
+      currentlyInfected: impactCurrentlyInfected
+    },
+    severe: {
+      currentlyInfected: severeCurrentlyInfected
+    }
+  };
+};
+const completeEstimator = (data) => {
+  const { impact, severe } = currentlyInfected(data);
+};
 const covid19ImpactEstimator = (data) => data;
 
 export default covid19ImpactEstimator;
