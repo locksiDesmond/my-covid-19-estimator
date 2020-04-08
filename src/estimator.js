589972d26c8eb500ef3333bbc5c60b92d1369a9b
LocksiDesmond <Des.olanrewaju@gmail.com>
2020-04-08 05:56:14 +0100

    completed Challenges 1 to 3

--- src/estimator.js
+++ src/estimator.js
@@ -3,6 +3,7 @@ const currentlyInfected = (data) => {
   const { reportedCases } = Inputdata;
   const impactCurrentlyInfected = reportedCases * 10;
   const severeCurrentlyInfected = reportedCases * 50;
+
   return {
     impact: {
       currentlyInfected: impactCurrentlyInfected
@@ -12,9 +13,51 @@ const currentlyInfected = (data) => {
     }
   };
 };
+const findInfectionsByRequestedTime = (object, data) => {
+  let numberOfDays;
+  const { periodType, timeToElapse, totalHospitalBeds } = data;
+  switch (periodType) {
+    case 'days':
+      numberOfDays = timeToElapse;
+      break;
+    case 'months':
+      numberOfDays = Number(timeToElapse) * 30;
+      break;
+    case 'weeks':
+      numberOfDays = Number(timeToElapse) * 7;
+      break;
+    default:
+      throw new Error('period type undefined');
+  }
+  const iBRT = object.currentlyInfected * 2 ** Math.floor(numberOfDays / 3);
+  // infectionsByRequested is solved here
+  object.infectionsByRequestedTime = iBRT;
+  // severeCasesByRequested Time is solved here
+  object.severeCaseByRequestedTime = iBRT * 0.15;
+  const hBBRT = 0.35 * totalHospitalBeds - object.severeCaseByRequestedTime;
+  // hospitalBedsByRequestedTime is solved here
+  object.hospitalBedsByRequestedTime = hBBRT;
+  const cFICUBRT = 0.05 * iBRT;
+  // cases For ICU BY RequestedTime is solved here;
+  object.casesForICUByRequestedTime = cFICUBRT;
+  const cFVBRT = iBRT * 0.02;
+  object.casesForVentilatorsByRequestedTime = cFVBRT;
+  // challenge 3
+  const { region } = data;
+  const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = region;
+  const dIF = avgDailyIncomeInUSD * avgDailyIncomePopulation * iBRT;
+  object.dollarsInFlight = dIF;
+};
 const completeEstimator = (data) => {
   const { impact, severe } = currentlyInfected(data);
+  findInfectionsByRequestedTime(impact, data);
+  findInfectionsByRequestedTime(severe, data);
+  return {
+    data,
+    impact,
+    severe
+  };
 };
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => completeEstimator(data);
 
 export default covid19ImpactEstimator;
