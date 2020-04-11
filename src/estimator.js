cc58895a186111806574ec62b75cc2b10224fb16
LocksiDesmond <Des.olanrewaju@gmail.com>
2020-04-11 05:41:29 +0100

    Math.trunc() dollars in flight

--- src/estimator.js
+++ src/estimator.js
@@ -47,7 +47,7 @@ const findInfectionsByRequestedTime = (object, data) => {
   const { region } = data;
   const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = region;
   const dIF = avgDailyIncomeInUSD * avgDailyIncomePopulation * iBRT;
-  object.dollarsInFlight = dIF * numberOfDays;
+  object.dollarsInFlight = Math.trunc(dIF * numberOfDays);
 };
 const completeEstimator = (data) => {
   const { impact, severe } = currentlyInfected(data);
